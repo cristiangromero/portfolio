ed6f9d95ba5c5bf3dfc824d711393fd360b30aa8
Criss <rcristian1986@gmail.com>
2022-03-01 18:30:25 -0300

    Dividido en componentes angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {ScriptLoaderService} from './script-loader.service';
+import { HeadComponent } from './components/head/head.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { LanguageComponent } from './components/language/language.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    HeroComponent,
+    AboutComponent,
+    SkillsComponent,
+    ResumeComponent,
+    LanguageComponent,
+    CoursesComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
