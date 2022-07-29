48ecd51623badcbe1881b5d53759d4da723ed7fb
Criss <rcristian1986@gmail.com>
2022-07-29 01:44:54 -0300

    CRUD SocialMedia funcionando correctamente

--- src/app/components/head/head.component.ts
+++ src/app/components/head/head.component.ts
@@ -13,92 +13,105 @@ import { AuthenticationService } from '../../services/auth.service';
   styleUrls: ['./head.component.scss']
 })
 export class HeadComponent implements OnInit {
-  
+
   public person: Person | undefined;
-  public social: SocialMedia[]=[];
+  public social: SocialMedia[] = [];
   public editPerson: Person | undefined;
   public editSocial: SocialMedia | undefined;
   public deleteSocial: SocialMedia | undefined;
-  socialMediaList:SocialMediaClass[]=[];
-  selected:SocialMediaClass= new SocialMediaClass;
+  public modeMethod: String | undefined;
+  socialMediaList: SocialMediaClass[] = [];
+  ngSelected: SocialMediaClass = new SocialMediaClass;
 
-  constructor(private headService: HeadService, private loggedService:AuthenticationService) { }
+  constructor(private headService: HeadService, private loggedService: AuthenticationService) { }
 
-  loggedIn(){
+  loggedIn() {
     return this.loggedService.isUserLoggedIn();
   }
 
   ngOnInit(): void {
     this.getPerson();
     this.getSocialMedia();
-    this.socialMediaList.push(new SocialMediaClass("linkedin","bx bxl-linkedin"));
-    this.socialMediaList.push(new SocialMediaClass("whatsapp","bx bxl-whatsapp"));
-    this.socialMediaList.push(new SocialMediaClass("skype","bx bxl-skype"));
-    this.socialMediaList.push(new SocialMediaClass("facebook","bx bxl-facebook"));
-    
-    }
+    this.socialMediaList.push(new SocialMediaClass("linkedin", "bx bxl-linkedin"));
+    this.socialMediaList.push(new SocialMediaClass("whatsapp", "bx bxl-whatsapp"));
+    this.socialMediaList.push(new SocialMediaClass("skype", "bx bxl-skype"));
+    this.socialMediaList.push(new SocialMediaClass("facebook", "bx bxl-facebook"));
 
-  public getPerson():void{
+  }
+
+  public getPerson(): void {
     this.headService.getPerson().subscribe({
-      next:(Response: Person) =>{
-        this.person=Response;
+      next: (Response: Person) => {
+        this.person = Response;
       },
-      error:(error:HttpErrorResponse) =>{
+      error: (error: HttpErrorResponse) => {
         alert(error.message);
       }
     });
   }
 
-  public getSocialMedia():void{
+  public getSocialMedia(): void {
     this.headService.getSocialMedia().subscribe({
-      next:(Response: SocialMedia[]) =>{
-        this.social=Response;
+      next: (Response: SocialMedia[]) => {
+        this.social = Response;
       },
-      error:(error:HttpErrorResponse) =>{
+      error: (error: HttpErrorResponse) => {
         alert(error.message);
       }
     });
   }
 
-  public onAddSocialMedia(addForm: NgForm){
-    addForm.value.icon="bx bxl-"+addForm.value.description;
-    addForm.value.person= this.person;
+  public onOpenModal(mode: String, socialMedia?: SocialMedia) {
+    if (mode === 'edit') {
+      this.editSocial = socialMedia;
+      this.modeMethod = mode;
+    }
+    else if (mode === 'delete') {
+      this.deleteSocial = socialMedia;
+    }
+  }
+  
+  public onAddSocialMedia(addForm: NgForm) {
+    addForm.value.icon = "bx bxl-" + addForm.value.description;
+    addForm.value.person = this.person;
     console.log(addForm.value);
     document.getElementById('add-socialmedia-form')?.click();
-    this.headService.addSocialMedia(addForm.value).subscribe({
-      next: (Response:SocialMedia) =>{
-        console.log(Response);
-        this.getSocialMedia();
-        addForm.reset();
-      },
-      error:(error:HttpErrorResponse)=>{
-        alert(error.message);
-        addForm.reset();
-      }
-    })
+    if (this.modeMethod === 'add') {
+      this.headService.addSocialMedia(addForm.value).subscribe({
+        next: (Response: SocialMedia) => {
+          console.log(Response);
+          this.getSocialMedia();
+          addForm.reset();
+        },
+        error: (error: HttpErrorResponse) => {
+          alert(error.message);
+          addForm.reset();
+        }
+      })
+    } else if (this.modeMethod === 'edit') {
+      addForm.value.idSocialMedia = this.editSocial?.idSocialMedia;
+      this.headService.updateSocialMedia(addForm.value).subscribe({
+        next: (Response: SocialMedia) => {
+          console.log(Response);
+          this.getSocialMedia();
+          addForm.reset();
+        },
+        error: (error: HttpErrorResponse) => {
+          alert(error.message);
+          addForm.reset();
+        }
+      })
+    }
+    this.modeMethod = 'add';
   }
 
-  public onUpdateSocialMedia(socialMedia: SocialMedia){
-    this.editSocial=socialMedia;
-    document.getElementById('add-socialmedia-form')?.click();
-    this.headService.updateSocialMedia(socialMedia).subscribe({
-      next: (Response:SocialMedia) =>{
-        console.log(Response)
-        this.getSocialMedia();
-      },
-      error:(error:HttpErrorResponse)=>{
-        alert(error.message);
-      }
-    })
-  }
-
-  public onDeleteSocialMedia(socialMediaId: number):void{
+  public onDeleteSocialMedia(socialMediaId: number): void {
     this.headService.deleteSocialMedia(socialMediaId).subscribe({
-      next: (Response:void) =>{
+      next: (Response: void) => {
         console.log(Response)
         this.getSocialMedia();
       },
-      error:(error:HttpErrorResponse)=>{
+      error: (error: HttpErrorResponse) => {
         alert(error.message);
       }
     })
